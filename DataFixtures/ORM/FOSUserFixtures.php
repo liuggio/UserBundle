185c1cfc36a2b085a826ab58e631500b6225e116
Gianluca Bargelli <g.bargelli@gmail.com>
2011-12-19 11:19:05 +0100

    Added encoders to Data Fixtures

--- DataFixtures/ORM/FOSUserFixtures.php
+++ DataFixtures/ORM/FOSUserFixtures.php
@@ -3,17 +3,27 @@
 namespace Tvision\Bundles\UserBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Tvision\Bundles\UserBundle\Entity\User;
 
-class FOSUserFixtures implements FixtureInterface
+class FOSUserFixtures implements FixtureInterface, ContainerAwareInterface
 {
+
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     public function load($em)
     {
         $user = new User();
         $user->setUsername('john');
-        $user->setPlainPassword('doe');
+        $encoder = $this->container->get('security.encoder_factory')->getEncoder($user);
+        $user->setPassword($encoder->encodePassword('doe', $user->getSalt()));
         $user->setEmail('john@doe.com');
-        $user->setAlgorithm('sha512');
         $user->setEnabled(true);
 
         $em->persist($user);
